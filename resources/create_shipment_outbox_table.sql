f50eecde4ba5dfc7e3a31d414082eac6786bfe83
odyildiz <ozgurdoganyildiz@hotmail.com>
2023-05-20 15:14:18 +0300

    Add docker compose and sql file for initializing kafka and postgresql

---
+++ resources/create_shipment_outbox_table.sql
@@ -0,0 +1,21 @@
+CREATE SCHEMA IF NOT EXISTS outbox;
+CREATE SEQUENCE IF NOT EXISTS outbox.shipment_outbox_id_seq;
+
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'shipment_outbox_state') THEN
+        CREATE TYPE outbox.shipment_outbox_state AS enum
+        (
+            'SUCCESS','FAIL','PENDING'
+        );
+    END IF;
+END$$;
+
+CREATE TABLE IF NOT EXISTS outbox.shipment_outbox (
+    id integer default nextval('outbox.shipment_outbox_id_seq'::regclass) not null
+                                                  constraint shipment_outbox_pkey
+                                                  primary key,
+                                                  queue_name text not null,
+                                                  outbox_entity jsonb not null,
+                                                  status outbox.shipment_outbox_state not null
+);
